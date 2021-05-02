2e93a80d46d8f2711fad9709b7d72966fc1935b8
alu0101105741 <alu0101105741@ull.edu.es>
2021-05-02 19:45:36 +0100

    Actualizando la rama del build.yml

--- test/ejercicio-2.spec.ts
+++ test/ejercicio-2.spec.ts
@@ -4,10 +4,6 @@ import {expect} from 'chai';
 import {pipe, noPipe} from '../src/ejercicio-2';
 
 describe('Test block on notes', () => {
-  it('Pipe method', () => {
-    expect(pipe('prueba.txt', true, true, true));
-  });
-
   it('No pipe method', () => {
     expect(2).to.be.equal(2);
   });
