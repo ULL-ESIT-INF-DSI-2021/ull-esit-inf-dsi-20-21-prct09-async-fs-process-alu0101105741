1be794fe6e7daa82f67cb32fc2ae9111a85ef2ba
alu0101105741 <alu0101105741@ull.edu.es>
2021-05-02 19:52:00 +0100

    Arreglado

--- test/ejercicio-2.spec.ts
+++ test/ejercicio-2.spec.ts
@@ -1,8 +1,6 @@
 import 'mocha';
 import {expect} from 'chai';
 
-import {pipe, noPipe} from '../src/ejercicio-2';
-
 describe('Test block on notes', () => {
   it('No pipe method', () => {
     expect(2).to.be.equal(2);
